8b8eebf65049fef48f0225c90e2278f2a6167bfb
Lucas Fabiano <p000lfabiano@prservicos.com.br>
2018-06-30 05:28:53 -0300

    menu e sticky:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    window.addEventListener('scroll', () => {
+      let sobre = document.getElementById("about");
+      let nabvar = document.getElementById("navbar-div");
+      let headerpos = sobre.offsetTop;;// - 65;
+      // console.log(headerpos);
+      // console.log(window.pageYOffset);
+      if (window.pageYOffset > headerpos) {
+        nabvar.classList.remove('position-absolute');
+        nabvar.classList.add('position-fixed');
+        nabvar.classList.remove('navbar-dark');
+        nabvar.classList.add('navbar-light');
+        nabvar.classList.add('bg-light');
+      } else {
+        nabvar.classList.add('position-absolute');
+        nabvar.classList.remove('position-fixed');
+        nabvar.classList.add('navbar-dark');
+        nabvar.classList.remove('navbar-light');
+        nabvar.classList.remove('bg-light');
+      }
+    });
+  }
 }
